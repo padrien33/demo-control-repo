4b85d5b1687638b84b1ec1419af4935646720293
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-10-20 11:09:31 +0100

    Update download_pe.sh

--- scripts/download_pe.sh
+++ scripts/download_pe.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-PE_VERSION=2017.3.0
-PE_SOURCE=puppet-enterprise-${PE_VERSION}-el-7-x86_64
+
+#Download PE, change version here
+PE_VERSION=2017.3.1
+PE_SOURCE=puppet-enterprise-${PE_VERSION}-ubuntu-16.04-amd64
 DOWNLOAD_URL=https://s3.amazonaws.com/pe-builds/released/${PE_VERSION}/${PE_SOURCE}.tar.gz
 wget --progress=bar ${DOWNLOAD_URL}
 wait
 tar zxf ${PE_SOURCE}.tar.gz
+wait
+
+#Setup password for the Web GUI and change the hostname to yours
+cd ${PE_SOURCE}
+echo '{' > pe.conf
+echo '  "console_admin_password": "",' >> pe.conf
+echo '  "puppet_enterprise::puppet_master_host": "",' >> pe.conf
+echo '}' >> pe.conf
+vim pe.conf
+
+
+
