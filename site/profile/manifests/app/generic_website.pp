87de5d6ddbac351b6c7699075428b03562796fa0
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-10-31 12:41:42 +0000

    Create generic_website.pp

---
+++ site/profile/manifests/app/generic_website.pp
@@ -0,0 +1,7 @@
+# Example cross platform generic website class
+class profile::app::generic_website {
+  case $::kernel {
+    'windows': { include profile::app::generic_website::windows }
+    'linux':   { include profile::app::generic_website::linux   }
+  }
+}
