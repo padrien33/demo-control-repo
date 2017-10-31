a013407bad705bb0b92671c479de1c7ed909e590
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-10-31 17:29:21 +0000

    Update fourthcoffeeIIS.pp

--- site/profile/manifests/app/fourthcoffeeIIS.pp
+++ site/profile/manifests/app/fourthcoffeeIIS.pp
@@ -1,9 +1,9 @@
 class profile::app::fourthcoffeeIIS(
 $iis_features = ['Web-WebServer','Web-Scripting-Tools']
-
+){
 iis_feature { $iis_features:
   ensure => 'present',
-){
+
 
 iis_site { 'minimal':
   ensure          => 'started',
