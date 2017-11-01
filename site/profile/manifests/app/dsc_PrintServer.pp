b56378915a2c6a9c05fed9aa4f5e85b58fa06367
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-11-01 10:23:54 +0000

    Update and rename dsc_windowsfeature.pp to dsc_PrintServer.pp

---
+++ site/profile/manifests/app/dsc_PrintServer.pp
@@ -0,0 +1,6 @@
+class profile::app::dsc_PrintServer{
+dsc_windowsfeature {'featureexample':
+  dsc_ensure = 'present'
+  dsc_name = 'Print-Server'
+  }
+}
--- site/profile/manifests/app/dsc_windowsfeature.pp
+++
@@ -1,4 +0,0 @@
-class profile::app::dsc_windowsfeature {'featureexample':
-  dsc_ensure = 'present'
-  dsc_name = 'Print-Server'
-}
