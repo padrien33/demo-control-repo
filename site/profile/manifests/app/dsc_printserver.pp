956452e147a25e9538c82b7837c58731a9074b6e
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-11-01 11:23:44 +0000

    Update dsc_printserver.pp

--- site/profile/manifests/app/dsc_printserver.pp
+++ site/profile/manifests/app/dsc_printserver.pp
@@ -2,5 +2,9 @@ class profile::app::dsc_printserver {
   dsc_windowsfeature {'Print':
     dsc_ensure => 'present',
     dsc_name   => 'Print-Server',
+  } 
+  dsc_windowsfeature {'IISConsole':
+    dsc_ensure => 'present',
+    dsc_name   => 'Web-Mgmt-Console',
   }
 }
