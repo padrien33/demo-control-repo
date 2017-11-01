4d9a9228fd8628a0fd1f512e2bf960d564d1b9de
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-11-01 10:36:24 +0000

    Update dsc_printserver.pp

--- site/profile/manifests/app/dsc_printserver.pp
+++ site/profile/manifests/app/dsc_printserver.pp
@@ -1,6 +1,6 @@
 class profile::app::dsc_printserver {
   dsc_windowsfeature {'featureexample':
-  dsc_ensure = 'present'
-  dsc_name = 'Print-Server'
+  dsc_ensure = 'present',
+  dsc_name = 'Print-Server',
   }
 }
