18a51337ed210d910cdee2690dc0e6c6a02850af
Pierre-Adrien Attouche <30621172+padrien33@users.noreply.github.com>
2017-11-05 17:50:45 +0000

    Create firewall.pp

---
+++ site/profile/manifests/firewall.pp
@@ -0,0 +1,11 @@
+### CUSTOM APPLICATION FIREWALL RULE
+  windows_firewall::exception { 'GSKrule':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'Allow',
+    enabled      => 'yes',
+    protocol     => 'TCP',
+    local_port   => '80',
+    display_name => 'GSK FireWall Rule',
+    description  => 'Inbound rule example for demo purposes',
+  }
