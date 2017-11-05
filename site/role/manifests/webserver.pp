b9c8480ddd913647021ae94e16d9fbcedf648560
Pierre-Adrien Attouche <30621172+padrien33@users.noreply.github.com>
2017-11-05 17:40:11 +0000

    Update webserver.pp

--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -2,7 +2,7 @@ class role::webserver {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
   #All roles should include the base profile
-  #include profile::base
+  include profile::base
   include profile::app::fourthcoffee
 
 }
