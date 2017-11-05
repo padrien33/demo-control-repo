d90336e0d100989334fc9ee9dce2d3d159de6e5b
Pierre-Adrien Attouche <30621172+padrien33@users.noreply.github.com>
2017-11-05 12:13:28 +0000

    Update windows_service.pp

--- site/profile/manifests/windows_service.pp
+++ site/profile/manifests/windows_service.pp
@@ -1,36 +1,54 @@
 class profile::windows_service {
+
+  # Disable Windows Push Notifications System Service
   service {'WpnService':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable Network Connection Broker 
   service {'NcbService':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable Print Spooler
   service {'Spooler':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable ActiveX Installer (AxInstSV)  
   service {'AxInstSV':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable Bluetooth Support Service  
   service {'bthserv':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable CDPUserSvc  
   service {'CDPUserSvc':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable Geolocation Service  
   service {'lfsvc':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable dmwappushsvc
   service {'dmwappushservice':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Downloaded Maps Manager  
   service {'MapsBroker':
     ensure => 'stopped',
     enable   => 'false',
