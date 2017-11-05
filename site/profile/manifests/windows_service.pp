edf0ec330f54eb7f5d008628e05774529352c503
Pierre-Adrien Attouche <30621172+padrien33@users.noreply.github.com>
2017-11-05 16:14:07 +0000

    Update windows_service.pp

--- site/profile/manifests/windows_service.pp
+++ site/profile/manifests/windows_service.pp
@@ -48,10 +48,192 @@ class profile::windows_service {
     enable   => 'false',
     }
     
-  # Downloaded Maps Manager  
+  # Disable Downloaded Maps Manager  
   service {'MapsBroker':
     ensure => 'stopped',
     enable   => 'false',
     }
+    
+  # Disable Contact Data
+  service {'PimIndexMaintenanceSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+        
+ # Disable Internet Connection Sharing (ICS)
+  service {'SharedAccess':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+    
+    
+     # Disable Link-Layer Topology Discovery Mapper
+  service {'lltdsvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+    
+  # Disable Microsoft Account Sign-in Assistant
+  service {'wlidsvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+    
+  # Disable Microsoft Account Sign-in Assistant
+  service {'NgcSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+    
+    
+  # Disable Microsoft Passport Container
+  service {'NgcCtnrSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+    
+  # Disable Phone Service
+  service {'PhoneSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }    
+
+  # Disable Printer Extensions and Notifications
+  service {'PrintNotify':
+    ensure => 'stopped',
+    enable   => 'false',
+    }
+
+  # Disable Program Compatibility Assistant Service
+  service {'PcaSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+
+  # Disable Quality Windows Audio Video Experience
+  service {'QWAVE':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+
+  # Disable Radio Management Service
+  service {'RmSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+    
+  # Disable Sensor Data Service
+  service {'SensorDataService':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+    
+  # Disable Sensor Monitoring Service
+  service {'SensrSvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }      
+
+  # Disable SensorService
+  service {'SensorService':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+  # Disable Shell Hardware Detection
+  service {'ShellHWDetection':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+  # Disable Smart Card Device Enumeration Service
+  service {'ScDeviceEnum':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+   # Disable SSDP Discovery
+  service {'SSDPSRV':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+   # Disable Still Image Acquisition Events
+  service {'WiaRpc':
+    ensure => 'stopped',
+    enable   => 'false',
+    }        
+
+   # Disable Touch Keyboard and Handwriting Panel Service
+  service {'TabletInputService':
+    ensure => 'stopped',
+    enable   => 'false',
+    }  
+    
+   # Disable UPnP Device Host
+  service {'upnphost':
+    ensure => 'stopped',
+    enable   => 'false',
+    }   
+    
+  # Disable User Data Access
+  service {'UserDataSvc_28b80f':
+    ensure => 'stopped',
+    enable   => 'false',
+    }   
+
+  # Disable User Data Storage
+  service {'UnistoreSvc_28b80f':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+  # Disable WalletService
+  service {'WalletService':
+    ensure => 'stopped',
+    enable   => 'false',
+    }      
+
+  # Disable Windows Audio
+  service {'Audiosrv':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+
+  # Disable Windows Audio Endpoint Builder
+  service {'AudioEndpointBuilder':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+  # Disable Windows Camera Frame Server
+  service {'FrameServer':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+  # Disable Windows Image Acquisition (WIA)
+  service {'stisvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+
+  # Disable Windows Insider Service
+  service {'wisvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+  # Disable Windows Mobile Hotspot Service
+  service {'icssvc':
+    ensure => 'stopped',
+    enable   => 'false',
+    } 
+    
+    
+   # Disable Windows Push Notifications User Service
+  service {'WpnUserService':
+    ensure => 'stopped',
+    enable   => 'false',
+    }    
+
 }
 
